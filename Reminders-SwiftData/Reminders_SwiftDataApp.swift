1784e5d837029ca16a2f4299a3be029de3159a02
Daniel Condly <settle.never@aol.com>
2024-09-25 13:46:52 -0400

    Initial Commit

---
+++ Reminders-SwiftData/Reminders_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_SwiftDataApp.swift
+//  Reminders-SwiftData
+//
+//  Created by DJC on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
